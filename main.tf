43c4714f959eadb866d5a12aa403068f3f0d716a
Igor Rodionov <goruha@gmail.com>
2017-06-27 18:04:42 +0600

    Use label module

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 # Define composite variables for resources
-resource "null_resource" "default" {
-  triggers = {
-    id = "${lower(format("%v-%v-%v", var.namespace, var.stage, var.name))}"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+module "label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=init"
+  namespace = "${var.namespace}"
+  name      = "${var.name}"
+  stage     = "${var.stage}"
 }
 
 resource "aws_elastic_beanstalk_application" "default" {
-  name        = "${null_resource.default.triggers.id}"
+  name        = "${module.label.value}"
   description = "${var.description}"
 }
