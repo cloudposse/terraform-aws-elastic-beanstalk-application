9016ee0521f7eb7f12f2ee0e4e9d770c5af4117b
Roman Pertl <533172+roock@users.noreply.github.com>
2019-06-06 11:13:01 +0200

    adding support for tags in AWS elasticbeanstalk application

--- main.tf
+++ main.tf
@@ -12,4 +12,9 @@ module "label" {
 resource "aws_elastic_beanstalk_application" "default" {
   name        = "${module.label.id}"
   description = "${var.description}"
+  tags        = "${module.label.tags}"
+  # because of https://github.com/terraform-providers/terraform-provider-aws/issues/3963
+  lifecycle {
+    ignore_changes = ["tags"]
+  }
 }
