8f8841452433fc539ff90ae31620e5ca941e60dc
vishal <vishal.bhogate07@gmail.com>
2020-03-10 11:34:19 +1100

    Git Elastic beanstalk Application

--- examples/complete/fixtures.us-east-2.tfvars
+++
@@ -1,9 +0,0 @@
-region = "us-east-2"
-
-namespace = "eg"
-
-stage = "test"
-
-name = "elastic-beanstalk-app"
-
-description = "Elastic Beanstalk Aplication test in us-east-2"
--- examples/complete/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "elastic_beanstalk_application_name" {
-  value       = module.elastic_beanstalk_application.elastic_beanstalk_application_name
-  description = "Elastic Beanstalk Application name"
-}
